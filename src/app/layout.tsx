b8d78e951a4af7de08074b408b538559c0a0006b
Youness Bendra <younesbendr@gmail.com>
2026-02-23 16:01:47 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
@@ -20,7 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased bg-background text-foreground selection:bg-primary/30">
+      <body className="font-body antialiased bg-background text-foreground selection:bg-primary/30" suppressHydrationWarning>
         {children}
         <Toaster />
       </body>
