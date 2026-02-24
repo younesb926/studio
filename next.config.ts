aaa52aa8a27e046ca725194f29aa3b16870ebf3f
Youness Bendra <younesbendr@gmail.com>
2026-02-24 18:03:02 +0000

    https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7nEaxripyy6WJOFgY

--- next.config.ts
+++ next.config.ts
@@ -65,6 +65,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
