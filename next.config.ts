e5c16b944b9a2697f2824038350e8bf4f5f5f5c1
Youness Bendra <younesbendr@gmail.com>
2026-02-24 14:03:30 +0000

    https://electromall.ma/wp-content/uploads/2024/09/Tv-SAMSUNG-QLED-QA85Q6

--- next.config.ts
+++ next.config.ts
@@ -41,6 +41,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'electromall.ma',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
