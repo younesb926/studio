e20bc6327d8eb91eeb1e42c486f0d45f6a6073a1
Youness Bendra <younesbendr@gmail.com>
2026-02-24 18:01:14 +0000

    https://www.corning.com/content/dam/corning/media/worldwide/csm/images/3

--- next.config.ts
+++ next.config.ts
@@ -59,6 +59,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.corning.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
