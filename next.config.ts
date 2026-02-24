1e9a04ef2057b2927f818259c3eacb99130b2de1
Youness Bendra <younesbendr@gmail.com>
2026-02-24 14:10:10 +0000

    https://imagedelivery.net/AZ5kNEcp8roCR6XQJU10qQ/5f788d55-dd93-498c-3b64

--- next.config.ts
+++ next.config.ts
@@ -47,6 +47,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'imagedelivery.net',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
