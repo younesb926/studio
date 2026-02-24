16cf6a76dc54e556681b48f1bcda964e6bc85a65
Youness Bendra <younesbendr@gmail.com>
2026-02-24 14:00:05 +0000

    https://www.pexels.com/fr-fr/photo/fleurs-retro-vintage-television-17233

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -31,6 +32,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.pexels.com',
         port: '',
         pathname: '/**',
       },
